4ab166bfa9bc6a5a4f7a639ad37349d97bb0370c
Aranganathan Rathinavelu <arangates@gmail.com>
2024-09-28 18:19:54 +0200

    Fix project initialization

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["class"],
-  content: [],
+  content: [
+    "./index.html", // Include the path to your main HTML file
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JavaScript and TypeScript files in src
+  ],
   theme: {
     extend: {
       borderRadius: {
